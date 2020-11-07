295523009cf213d730942a2870cf409e6a8acb68
harshil-patel-coder <harshil881994@gmail.com>
2020-11-07 17:31:47 +0530

    commit code

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0972d7f9f48fcf0617bf610d12e89755'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '16ec186b-7089-412f-bf36-4473541720d0'
+};
